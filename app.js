85b856f1ad7dcb4a47d983ef7d27d53d92fa4af4
ChengyuanCai <kevin.c.cai@gatech.edu>
2018-05-28 22:47:40 -0400

    connect to database

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database')
 
-mongoose.connect();
+// Connect to database
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+	console.log("connected to database " + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+	console.log("databse error " + err);
+});
 
 const app = express();
 
@@ -33,5 +43,5 @@ app.get('/', (req, res) => {
 
 // Start Server
 app.listen(port, () => {
-	console.log("Server started on port" + port);
+	console.log("Server started on port " + port);
 });
