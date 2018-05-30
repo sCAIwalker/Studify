c3a6d82b8eb44f78479095c2e7215f1908936088
ChengyuanCai <kevin.c.cai@gatech.edu>
2018-05-29 22:47:21 -0400

    finish registration system

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user')
 
 // Register
-router.get('/register', (req, res, next) => {
-	res.send("register");
+router.post('/register', (req, res, next) => {
+	let newUser = new User({
+		name: req.body.name,
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password
+	});
+
+	User.addUser(newUser, (err, user) => {
+		if (err) {
+			res.json({success: false, msg:'failed to register user'});
+		} else {
+			res.json({success: true, msg:'user registered'});
+		}
+	});
 });
 
 // Authentication
-router.get('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
 	res.send("authenticate");
 });
 
 // Profile 
 router.get('/profile', (req, res, next) => {
 	res.send("profile");
-});
-
-// Validate
- router.get('/validate', (req, res, next) => {
-	res.send("validate");
 });
 
 module.exports = router;
