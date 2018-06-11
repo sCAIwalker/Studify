8ce7abf90dcea4ab96a9b5aa5db811da547db7e6
ChengyuanCai <kevin.c.cai@gatech.edu>
2018-06-10 21:33:25 -0400

    add option to choose between sound effects

--- angular-src/src/app/components/music/music.component.ts
+++ angular-src/src/app/components/music/music.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./music.component.css']
 })
 export class MusicComponent implements OnInit {
-
+  showRain : boolean;
+  showFireplace : boolean;
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  clickedRain() {
+    console.log("rain");
+    this.showRain = true;
+    this.showFireplace = false;
+  }
+
+  clickedFireplace() {
+    console.log("fireplace");
+    this.showRain = false;
+    this.showFireplace = true;
+  }
 }
